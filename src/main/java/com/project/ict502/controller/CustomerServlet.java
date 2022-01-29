1867cccba8a907d330cd0010dcd953f9f99a44e7
Aiman Anizan <anizanaimann1@gmail.com>
2022-01-29 17:39:14 +0800

    Add return for null action

--- src/main/java/com/project/ict502/controller/CustomerServlet.java
+++ src/main/java/com/project/ict502/controller/CustomerServlet.java
@@ -30,6 +30,8 @@ public class CustomerServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String action = request.getParameter("action");
 
+        if(action == null) return;
+
         switch (action.toLowerCase()) {
             case "signup":
                 signup(request, response);
