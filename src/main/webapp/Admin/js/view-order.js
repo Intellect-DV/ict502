6678d995d4496b32b793e4f858078c413722a884
Aiman Anizan <anizanaimann1@gmail.com>
2022-02-15 01:22:47 +0800

    Add filter button handler

--- src/main/webapp/Admin/js/view-order.js
+++ src/main/webapp/Admin/js/view-order.js
@@ -1,5 +1,22 @@
 const navItem = document.querySelector(".navigation__item[href='./view-order.jsp']");
+const filterItems = document.querySelectorAll(".filter__item > button");
 
 window.addEventListener("DOMContentLoaded", () => {
-    navItem.classList.add("active")
+    navItem.classList.add("active");
+
+    for(let filterItem of filterItems) {
+        filterItem.addEventListener("click", (event) => {
+            filterHandler(event);
+        })
+    }
 })
+
+const filterHandler = (event) => {
+    const currentBtn = event.target;
+
+    for(let filterItem of filterItems) {
+        filterItem.className = "";
+    }
+
+    currentBtn.classList.add("active");
+}
