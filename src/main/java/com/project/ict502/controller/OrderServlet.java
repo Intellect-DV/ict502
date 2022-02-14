64b5b346a82db28653f3f71a5af11742c15e7d02
Aiman Anizan <anizanaimann1@gmail.com>
2022-02-15 07:26:13 +0800

    Add method to change order status

--- src/main/java/com/project/ict502/controller/OrderServlet.java
+++ src/main/java/com/project/ict502/controller/OrderServlet.java
@@ -2,6 +2,7 @@ package com.project.ict502.controller;
 
 import com.project.ict502.dataaccess.OrderDA;
 import com.project.ict502.model.Customer;
+import com.project.ict502.model.Order;
 import com.project.ict502.model.Worker;
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -41,7 +42,15 @@ public class OrderServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String action = request.getParameter("action");
 
+        if(action == null) return;
+
+        switch(action.toLowerCase()) {
+            case "changestatus":
+                changeOrderStatus(request, response);
+                break;
+        }
     }
 
     private void retrieveForCust(HttpServletRequest request, HttpServletResponse response) {
@@ -89,6 +98,52 @@ public class OrderServlet extends HttpServlet {
         }
 
         json.put("orders", orders);
+        jsonResponse(response, 200, json);
+    }
+
+    private void changeOrderStatus(HttpServletRequest request, HttpServletResponse response) {
+        JSONObject json = new JSONObject();
+
+        HttpSession session = request.getSession(false);
+
+        if(session == null || session.getAttribute("workerObj") == null){
+            json.put("error", "Please login first");
+            jsonResponse(response, 400, json);
+            return;
+        }
+
+        String tempOrderId = request.getParameter("orderId");
+        String orderStatus = request.getParameter("orderStatus");
+
+        if(tempOrderId == null || orderStatus == null || tempOrderId.equals("") || orderStatus.equals("")) {
+            json.put("error", "Order id / status is empty");
+            jsonResponse(response, 400, json);
+            return;
+        }
+
+        int orderId = -1;
+
+        try {
+            orderId = Integer.parseInt(tempOrderId);
+        } catch (Exception err) {
+            err.printStackTrace();
+        }
+
+        if(orderId <= 0) {
+            json.put("error", "Order id / status is invalid format");
+            jsonResponse(response, 400, json);
+            return;
+        }
+
+        boolean succeed = OrderDA.updateOrderStatus(orderId, orderStatus);
+
+        if(!succeed) {
+            json.put("error", "Cannot update status");
+            jsonResponse(response, 400, json);
+            return;
+        }
+
+        json.put("message", "Order status updated");
         jsonResponse(response, 200, json);
     }
 }
