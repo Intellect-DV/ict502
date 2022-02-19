13fc0183a755cfb2235be334ed098aa1a63c9ce1
Aiman Anizan <anizanaimann1@gmail.com>
2022-02-19 21:46:37 +0800

    Add script for `payment`

---
+++ src/main/webapp/Customer/js/payment.js
@@ -0,0 +1,7 @@
+const modalInfo = document.querySelector(".modal__info");
+const modalCard = document.querySelector(".modal__card");
+const modalContent = document.querySelector(".modal__content");
+const modalClose = document.querySelector(".modal__close");
+
+const modalBackdrop = document.querySelector(".modal__backdrop");
+const modalBtnYes = document.querySelector(".action > .btn-confirm.green");
