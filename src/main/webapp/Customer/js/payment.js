5a0580ab4b17af84b557ede830d35b0b6eee9612
Aiman Anizan <anizanaimann1@gmail.com>
2022-02-19 22:07:22 +0800

    Add method to retrieve html credit-debit

--- src/main/webapp/Customer/js/payment.js
+++ src/main/webapp/Customer/js/payment.js
@@ -1,3 +1,5 @@
+const paymentContent = document.querySelector(".payment__content");
+
 const modalInfo = document.querySelector(".modal__info");
 const modalCard = document.querySelector(".modal__card");
 const modalContent = document.querySelector(".modal__content");
@@ -5,3 +7,17 @@ const modalClose = document.querySelector(".modal__close");
 
 const modalBackdrop = document.querySelector(".modal__backdrop");
 const modalBtnYes = document.querySelector(".action > .btn-confirm.green");
+
+window.addEventListener("DOMContentLoaded", () => {
+    getCreditDebitForm()
+})
+
+const getCreditDebitForm = () => {
+   axios.get("component/payment__credit_debit.html")
+       .then(res => {
+           paymentContent.innerHTML = res.data;
+        })
+       .catch(err => {
+            console.log(err.response.data);
+       })
+}
